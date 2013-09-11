3ca56f46febbc3bc7877f7198bb2f4ee967258e2
Артем <artygeneration@gmail.com>
2013-09-11 12:16:20 +0400

    причесывание

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php
     // Определение параметров базы данных по умолчанию
-    if ($_SERVER[REMOTE_ADDR]=="127.0.0.1") {   // подключение с локального компьютера - отладка на локальной машине
+    if ($_SERVER['REMOTE_ADDR']=="127.0.0.1") {   // подключение с локального компьютера - отладка на локальной машине
         define("const_dbName","startstroy");
         define("const_hostName","localhost");
         define("const_userName","root");
