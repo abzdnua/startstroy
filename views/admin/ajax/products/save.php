b0ee8a242aedc77d938855ae8a80e179ba6858ee
Артем <artygeneration@gmail.com>
2013-09-11 09:50:16 +0400

    причесывание

--- views/admin/ajax/products/save.php
+++ views/admin/ajax/products/save.php
@@ -9,51 +9,7 @@ $pathToSaveImg = $root.'/img/products/';
 $db = db::getInstance();
 
 if(!empty($_POST)){
-
-    $name = $_POST['product_name'];
-    $des = $_POST['product_des'];
-    $price = ($_POST['product_price'])?$_POST['product_price']:0;
-    $priceForSale = $_POST['product_priceforsale'];
-    $user = $_SESSION['userID'];
-    $date = date('Y-m-d H:i:s');
-    $category_id = $_POST['category_id'];
-    $subcategory_id = $_POST['subCategory_id'];
-    $material_id = $_POST['material'];
-    $firm_id = $_POST['firm'];
-    $show = ($_POST['show']=='on')?1:0;
-    $product_img_val = $_POST['product_img_val'];
-    if($_POST['id']!=''){
-        $id = $_POST['id'];
-        $sql = "UPDATE products SET
-                name = '{$name}',
-                des = '{$des}',
-                price = {$price},
-                priceForSale = {$priceForSale},
-                category_id = {$category_id},
-                subCategory_id = {$subcategory_id},
-                material_id = {$material_id},
-                firm_id = {$firm_id},
-                `show` = {$show},
-                userUpdate = {$user},
-                dateUpdate = '{$date}'
-                WHERE id = {$id}";
-     //  echo "error: ".$sql;
-        $db->query($sql);
-
-    }else{
-        $sql = "INSERT INTO products
-                (name,des,price,img,priceForSale,category_id,subCategory_id,material_id,firm_id,`show`,userCreate,dateCreate)
-                VALUES
-                ('{$name}','{$des}',{$price},'{$product_img_val}',{$priceForSale},{$category_id},{$subcategory_id},{$material_id},{$firm_id},{$show},{$user},'{$date}')";
-//       echo $sql;
-        $db->query($sql);
-        $id = $db->last();
-
-
-
-
-    }
-
+    $img_str = $_POST['product_img_val'];
     if(!empty($_FILES['product_img']['name'])!=''){
         if($_FILES['product_img']['size']/1024/1024>ini_get('upload_max_filesize')){
             $db->query("DELETE FROM products WHERE id={$id}");
@@ -62,9 +18,8 @@ if(!empty($_POST)){
         }
         set_time_limit(0);
         $img = new Upload($_FILES['product_img']);
-        if(($img->file_src_size/1024/1024>ini_get('upload_max_filesize'))or ($img->image_src_x < 330) or ($img->image_src_y < 330)){
+        if(($img->file_src_size/1024/1024>ini_get('upload_max_filesize'))or ($img->image_src_x>$img->image_src_y and $img->image_src_x < 330) or ($img->image_src_x<$img->image_src_y and $img->image_src_y < 330)){
 
-            $db->query("DELETE FROM products WHERE id={$id}");
             echo "\nЗагружаемое изображение должно быть не менее 330px по большей стороне и не более ".ini_get('upload_max_filesize');
             exit();
         }
@@ -73,13 +28,13 @@ if(!empty($_POST)){
         $img -> file_new_name_body = $uniq_img;
         $img -> jpeg_quality = 100;
 //        echo $img->image_src_x.' '.$img->image_src_y;
-            if($img->image_src_x>=$img->image_src_y){
-                $img->image_x = 330;
-                $img->image_ratio_y = true;
-            }else{
-                $img->image_y = 330;
-                $img->image_ratio_x = true;
-            }
+        if($img->image_src_x>=$img->image_src_y){
+            $img->image_x = 330;
+            $img->image_ratio_y = true;
+        }else{
+            $img->image_y = 330;
+            $img->image_ratio_x = true;
+        }
 //        echo $img->image_x.'  '.$img->image_y;
         $img->image_resize = true;
         $img -> process($pathToSaveImg);
@@ -97,13 +52,53 @@ if(!empty($_POST)){
         $img ->image_resize = true;
         $img -> process($pathToSaveImg);
         $thumb_str = 'm_'.$uniq_img.'.'.$img->image_src_type;
-
-        $sql = "UPDATE products SET
-                img = '{$img_str}'
-                WHERE id = {$id}";
-     //   echo "error: ".$sql;
-        $db->query($sql);
     }
+
+    $name = $_POST['product_name'];
+    $des = $_POST['product_des'];
+    $price = ($_POST['product_price'])?$_POST['product_price']:0;
+    $priceForSale = $_POST['product_priceforsale'];
+    $user = $_SESSION['userID'];
+    $date = date('Y-m-d H:i:s');
+    $category_id = $_POST['category_id'];
+    $subcategory_id = $_POST['subCategory_id'];
+    $material_id = $_POST['material'];
+    $firm_id = $_POST['firm'];
+    $show = ($_POST['show']=='on')?1:0;
+
+    if($_POST['id']!=''){
+        $id = $_POST['id'];
+        $sql = "UPDATE products SET
+                name = '{$name}',
+                des = '{$des}',
+                price = {$price},
+                img = '{$img_str}'
+                priceForSale = {$priceForSale},
+                category_id = {$category_id},
+                subCategory_id = {$subcategory_id},
+                material_id = {$material_id},
+                firm_id = {$firm_id},
+                `show` = {$show},
+                userUpdate = {$user},
+                dateUpdate = '{$date}'
+                WHERE id = {$id}";
+     //  echo "error: ".$sql;
+        $db->query($sql);
+
+    }else{
+        $sql = "INSERT INTO products
+                (name,des,price,img,priceForSale,category_id,subCategory_id,material_id,firm_id,`show`,userCreate,dateCreate)
+                VALUES
+                ('{$name}','{$des}',{$price},'{$img_str}',{$priceForSale},{$category_id},{$subcategory_id},{$material_id},{$firm_id},{$show},{$user},'{$date}')";
+//       echo $sql;
+        $db->query($sql);
+        $id = $db->last();
+
+
+
+
+    }
+
     if($_POST['char_count'] > 0)
     {
 
